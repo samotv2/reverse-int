1ee3d2590a16da2d6b63fe48c6ddd7643ff90fa5
unknown <samotv82@gmail.com>
2020-02-28 01:31:13 +0300

    fix: add

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 module.exports = function reverse (n) {
     var res = 0;
-        
-        while(n) {
-            res *= 10;
-            res = res + n % 10;
-            n= Math.floor(n/10);
+   
+   while (Math.abs(n/10)>0)
+   {
+            a =n%10;
+            n=Math.floor(n/10);
+            res=res*10+a;
         }
+    
         
         return (Math.abs(res));
  }
